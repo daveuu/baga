b28faf47ec97c7426df93c374f47503b4f034fbc
daveuu <daveuu@users.noreply.github.com>
2015-11-24 23:07:42 +0000

    finish making commandlines consistent (finishing:6cd644d841ac76b11023aa3491a1a22cca8cd6af)

--- ReadsAnalysis.py
+++ ReadsAnalysis.py
@@ -2,5 +2,5 @@ print('''
 test for linkage of variants on reads is available in CallVariants module
 e.g.
 baga_cli.py CheckLinkage --help
-baga_cli.py CheckLinkage --alignment_paths bams --vcf_paths vcfs --genome_name NC_002516.2 --check
+baga_cli.py CheckLinkage --alignment_paths bams --vcfs_paths vcfs --genome_name NC_002516.2 --check
 ''')
