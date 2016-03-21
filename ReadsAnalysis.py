d4bbb5b35152256efcd8e7815464096e5a7cc55c
daveuu <daveuu@users.noreply.github.com>
2016-03-21 14:09:00 +0000

    removed temporary ReadsAnalysis.py module

--- ReadsAnalysis.py
+++
@@ -1,6 +0,0 @@
-print('''
-test for linkage of variants on reads is available in CallVariants module
-e.g.
-baga_cli.py CheckLinkage --help
-baga_cli.py CheckLinkage --alignment_paths bams --vcfs_paths vcfs --genome_name NC_002516.2 --check
-''')
